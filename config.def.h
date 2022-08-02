62abcfad7c0a40ad533d20b3b7f6bc9b59ceac81
Nehemias44 <leandro.nehemias@gmail.com>
2022-08-02 10:53:25 -0300

    Update files.

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Nerd Font:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 20;
+static char *font = "Iosevka Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static int borderpx = 30;
 
 /*
  * What program is execed by st depends of these precedence rules:
